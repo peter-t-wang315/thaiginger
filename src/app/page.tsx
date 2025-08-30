c87046000ec99148795fd35c545227a674ef19e3
PeterWang <wang.t.peter@gmail.com>
2025-08-29 22:22:52 -0700

    Added buttons. Gotta figure out the time and hours...

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ gsap.registerPlugin(ScrollTrigger);
 export default function HomePage() {
   return (
     <main className="flex flex-col flex-1">
-      <div className="flex min-h-screen items-center justify-center">
+      <div className="flex flex-col min-h-screen items-center justify-center">
         <div className="flex flex-col items-center justify-center space-y-8">
           <h1 className="text-9xl text-primary">THAI GINGER</h1>
           <h1 className="text-8xl font-thai text-primary">ขิงไทย</h1>
@@ -27,11 +27,18 @@ export default function HomePage() {
             className="-mt-10"
           />
           <div className="flex w-[80%] justify-between">
-            <h2>Menu</h2>
-            <h2>Call</h2>
+            <Button variant="link">Menu</Button>
+            <Button variant="link" className="underline-swipe-right">
+              Call
+            </Button>
           </div>
         </div>
+        <div className="justify-self-end">
+          <h3>Tuesday - Sunday</h3>
+          <h3>11AM - 9PM</h3>
+        </div>
       </div>
+      <div className="flex min-h-screen"></div>
     </main>
   );
 }
