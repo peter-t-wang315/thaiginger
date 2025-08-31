43b7365949f642ddc96ed7ba95b712abe92092f3
PeterWang <wang.t.peter@gmail.com>
2025-08-31 00:50:23 -0700

    Updating the hover on tthe links.

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -19,7 +19,7 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80",
         ghost:
           "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
-        link: "underline-swipe text-3xl leading-9 font-bold text-secondary font-herculanum",
+        link: "underline-swipe cursor-pointer leading-9 font-bold text-secondary font-herculanum",
       },
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
