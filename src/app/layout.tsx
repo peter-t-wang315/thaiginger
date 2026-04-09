00422c4435c948eeca55e7c96db7710336491738
PeterWang <wang.t.peter@gmail.com>
2026-04-08 20:42:56 -0700

    PLEASAAAAAAAAAAASE

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,12 +3,6 @@ import { Noto_Sans_Thai } from "next/font/google";
 import localFont from "next/font/local";
 import "./globals.css";
 
-const herculanum = localFont({
-  src: "../../public/HerculanumLTProRoman.ttf",
-  display: "block", // prevent fallback rendering
-  variable: "--font-herculanum",
-});
-
 const notoThai = Noto_Sans_Thai({
   subsets: ["thai"], // ✅ makes sure Thai glyphs are included
   weight: ["400", "700"], // pick the ones you need
